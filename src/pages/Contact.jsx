35bddc728b2d1eb5311c4acc7d036fde05f49b37
Doondy <2000030173@kluniversity.in>
2025-10-25 23:10:47 +0530

    changed contact import

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "../styles/contact.css";
+import "../styles/Contact.css";
 
 export default function Contact() {
   const [form, setForm] = useState({ name: "", email: "", message: "" });
