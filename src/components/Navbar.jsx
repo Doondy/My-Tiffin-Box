36359822af040c6d15877d22cfebb88a9638df5d
Doondy <2000030173@kluniversity.in>
2025-10-25 23:09:31 +0530

    changed logo import

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../styles/Navbar.css";
-import logo from "../assets/logo.png";
+import logo from "../assets/Logo.png";
 
 export default function Navbar() {
   return (
